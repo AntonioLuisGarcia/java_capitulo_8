828fbd5c7fdfdb9585e5564a2c4d56616be3dc80
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-12-02 14:09:23 +0100

    ejercicio 15

---
+++ ejercicio15.java
@@ -0,0 +1,20 @@
+/*
+ *Ejercicio 15
+ *Muestra los números primos que hay entre 1 y 1000.
+ *
+ * @author Antonio Luis Garcia
+ */
+import matematicas.Varias;
+public class ejercicio15{
+   public static void main(String[] args){
+    int contador=0;
+    int numero=1;
+    while(contador<1000){
+        numero++;
+        if(Varias.esPrimo(numero)){
+            System.out.print(numero+", ");
+        }
+        contador++;
+    }
+}
+}
