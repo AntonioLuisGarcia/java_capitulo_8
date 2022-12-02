7cefe26c558a10d50470a4388d7ef59de8ab5a6a
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-12-02 14:12:29 +0100

    ejercicio 16 acabado

--- ejercicio16.java
+++ ejercicio16.java
@@ -1,17 +1,17 @@
 /*
- *Ejercicio 15
- *Muestra los números primos que hay entre 1 y 1000.
+ *Ejercicio 16
+ *Muestra los números capicúa que hay entre 1 y 99999.
  *
  * @author Antonio Luis Garcia
  */
 import matematicas.Varias;
-public class ejercicio15{
+public class ejercicio16{
    public static void main(String[] args){
     int contador=0;
     int numero=1;
-    while(contador<99999){
+    while(contador<1000){
         numero++;
-        if(Varias.esCapicua(numero)){
+        if(Varias.esPrimo(numero)){
             System.out.print(numero+", ");
         }
         contador++;
