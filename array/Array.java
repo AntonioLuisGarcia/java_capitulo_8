d9a62b389b67b5083ec9b90a6086e8f91a6b8c0f
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-12-06 13:02:50 +0100

    funcion generaArrayInt

--- array/Array.java
+++ array/Array.java
@@ -8,4 +8,26 @@ public class Array{
         }
         System.out.println();
     }
+
+    public static void muestraArray(long x[]){
+        for(int i=0;i<x.length;i++){
+            System.out.print(x[i]);
+        }
+        System.out.println();
+    }
+
+
+
+    public static int[] generaArrayInt(int n, int min, int max){
+
+        int[] array=new int[n];
+
+        for(int i=0;i<n;i++){
+            array[i]=(int)(Math.random()*(max-min+1)+min);
+        }
+
+        return array;
+        
+    }
+
 }
