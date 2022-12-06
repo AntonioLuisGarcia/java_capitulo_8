158ce1140458b227facba3f0b52e19ec0d7ace82
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-12-06 12:53:02 +0100

    funcion de mostrar array

---
+++ array/Array.java
@@ -0,0 +1,11 @@
+package array;
+
+public class Array{
+
+    public static void muestraArray(int x[]){
+        for(int i=0;i<x.length;i++){
+            System.out.print(x[i]);
+        }
+        System.out.println();
+    }
+}
