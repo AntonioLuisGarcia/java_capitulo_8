cd3bd99b88e24452c95cca627571616014728b4b
Antonio Luis Garcia <antonlugar777@gmail.com>
2022-12-12 08:25:42 +0100

    ejercicio 16

--- ejercicio16.java
+++ ejercicio16.java
@@ -4,14 +4,15 @@
  *
  * @author Antonio Luis Garcia
  */
-import matematicas.Varias;
+
+ import matematicas.Varias;
 public class ejercicio16{
    public static void main(String[] args){
     int contador=0;
     int numero=1;
-    while(contador<1000){
+    while(contador<99999){
         numero++;
-        if(Varias.esPrimo(numero)){
+        if(Varias.esCapicua(numero)){
             System.out.print(numero+", ");
         }
         contador++;
